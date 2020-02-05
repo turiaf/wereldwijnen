edfb27e7dbb4348c410a5d3451290bc51a83fe64
Turiaf Kouakanou <turiaf@gmail.com>
2020-02-05 13:44:56 +0100

    first commit test werelwijnen

---
+++ src/main/java/be/vdab/wereldwijnen/WereldwijnenApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.wereldwijnen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WereldwijnenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WereldwijnenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/wereldwijnen/WereldwijnenApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.wereldwijnen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WereldwijnenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
